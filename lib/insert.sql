d9ecd066f41c5fb5f24ebc925a7d7deb61e44797
szrharrison <s.z.r.harrison@gmail.com>
2017-03-20 16:07:49 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+insert into bears (name,gender,color,temperament,alive)
+  values ('Mr. Chocolate','M','brown','Sweet','true');
+insert into bears (name,gender,color,temperament,alive)
+  values ('Rowdy','M','black','Rowdy','true');
+insert into bears (name,gender,color,temperament,alive)
+  values ('Tabitha','F','brown','Sleepy','true');
+insert into bears (name,gender,color,temperament,alive)
+  values ('Sergeant Brown','M','brown','Terse','true');
+insert into bears (name,gender,color,temperament,alive)
+  values ('Melissa','F','brown','Hungry','true');
+insert into bears (name,gender,color,temperament,alive)
+  values ('Grinch','M','green','Mean One','true');
+insert into bears (name,gender,color,temperament,alive)
+  values ('Wendy','F','brown','Kind','true');
+insert into bears (gender,color,temperament,alive)
+  values ('M','brown','Grizly','true');
